bb3778e4f41b18be79b1e564ab76600283bad579
global-roam <global-roam@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-10 00:35:54 +0000

    initial commit
    git-svn-id: https://plugins.svn.wordpress.org/region-generation-by-fuel-type-widget/trunk@1195931 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ region-generation-by-fuel-type-widget.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: Region Generation By Fuel Type Widget
+Plugin URI: http://reneweconomy.com.au/
+Description: Use [region-generation-by-fuel-type-widget token='your-api-token'] to insert this widget into your page.
+Author: Global-Roam
+Author URI: http://www.global-roam.com/
+Version: 1.0
+*/
+
+add_shortcode( 'region-generation-by-fuel-type-widget', 'region_generation_by_fuel_type_widget' );
+
+function region_generation_by_fuel_type_widget( $atts ) {
+	
+	// short code attributes can only contain upper and lowercase letters, digits and underscores
+	// https://codex.wordpress.org/Shortcode_API
+	$apiToken = $atts['token'];  
+
+	$shortcode_text = '<div id="gr-widget-region-generation-by-fuel-type"></div>';
+	$shortcode_text .= '<script id="gr-widget-region-generation-by-fuel-type-script" ';
+	$shortcode_text .= 'data-api-token="' . $apiToken . '" ';
+	$shortcode_text .= 'src="http://widgets.global-roam.com/widgets/regionGenerationByFuelType" ';
+	$shortcode_text .= 'type="text/javascript">';
+	$shortcode_text .= '</script>';
+
+	return $shortcode_text;
+}
